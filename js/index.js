da5fd5e4d2dfe23458166678b1feeb87920af028
David <davidostos2@gmail.com>
2023-05-28 21:47:15 -0400

    acomodando url de redireccion

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const btn = document.getElementById('button__play');
-const page2 = "../pages/app.html"
+const page2 = "./pages/app.html"
 
 btn.addEventListener('click', () => {
   location.href = page2;
